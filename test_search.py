c139d9ddc42f8b149a218984377b71e9a9c63c63
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-06-17 17:05:29 -0500

    Added min/max price filters to the query

--- test_search.py
+++ test_search.py
@@ -7,7 +7,21 @@ from pprint import pprint
 try:
     api = Finding(appid=PRODUCTION_APPID, config_file=None)
     query = {
-        'keywords': '+Hammond +(a100,""a-100"",B2,""B-2"",B3,""B-3"",C2,""C-2"",C3,""C-3"")'
+        'keywords': '+Hammond +(a100,""a-100"",B2,""B-2"",B3,""B-3"",C2,""C-2"",C3,""C-3"")',
+        'itemFilter': [
+            {
+                'paramName': 'Currency',
+                'paramValue': 'USD',
+                'name': 'MaxPrice',
+                'value': '15000'
+            },
+            {
+                'paramName': 'Currency',
+                'paramValue': 'USD',
+                'name': 'MinPrice',
+                'value': '500'
+            },
+        ]
     }
 
     response = api.execute('findItemsAdvanced', query)
