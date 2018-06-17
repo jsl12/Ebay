03bd6dd4c5255ef1b3953d39c01c6690e0d8099d
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-06-17 17:38:06 -0500

    Moved the query construction out of the test_search.py file

--- test_search.py
+++ test_search.py
@@ -1,30 +1,12 @@
 from ebaysdk.finding import Connection as Finding
 from ebaysdk.exception import ConnectionError
 from accounts import PRODUCTION_APPID
+from hammond_query import query
 
 from pprint import pprint
 
 try:
     api = Finding(appid=PRODUCTION_APPID, config_file=None)
-    query = {
-        'keywords': '+Hammond +(a100,""a-100"",B2,""B-2"",B3,""B-3"",C2,""C-2"",C3,""C-3"")',
-        'categoryId': '180010',
-        'itemFilter': [
-            {
-                'paramName': 'Currency',
-                'paramValue': 'USD',
-                'name': 'MaxPrice',
-                'value': '15000'
-            },
-            {
-                'paramName': 'Currency',
-                'paramValue': 'USD',
-                'name': 'MinPrice',
-                'value': '500'
-            },
-        ]
-    }
-
     response = api.execute('findItemsAdvanced', query)
     results = response.dict()['searchResult']['item']
     for item in results:
