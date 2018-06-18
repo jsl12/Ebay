48040048f7f46d8bd9cc6a5934853f090ec47273
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-06-17 22:25:50 -0500

    Added selling state

--- ebay_processing.py
+++ ebay_processing.py
@@ -7,6 +7,8 @@ def response_to_dataframe(response):
     df['Item ID'] = pd.Series([item['itemId'] for item in results])
     df['Title'] = pd.Series([item['title'] for item in results])
     df['Current Price'] = pd.Series([float(item['sellingStatus']['currentPrice']['value']) for item in results])
+    # df['Bid Count'] = pd.Series([item['sellingStatus']['bidCount'] for item in results])
+    df['Selling State'] = pd.Series([item['sellingStatus']['sellingState'] for item in results])
     df['Location'] = pd.Series([item['location'] for item in results])
     df['URL'] = pd.Series([item['viewItemURL'] for item in results])
 
