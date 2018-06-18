3b4e2e9ad625cf0c5cf98e6949e24caf1077cdd3
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-06-17 21:44:50 -0500

    Changed how the DataFrame was being constructed to control the column order

--- ebay_processing.py
+++ ebay_processing.py
@@ -2,9 +2,12 @@ import pandas as pd
 
 def response_to_dataframe(response):
     results = response.dict()['searchResult']['item']
-    df = pd.DataFrame({
-        'Item ID': [item['itemId'] for item in results],
-        'Title': [item['title'] for item in results],
-        'Current Price': [float(item['sellingStatus']['currentPrice']['value']) for item in results]
-    })
+
+    df = pd.DataFrame()
+    df['Item ID'] = pd.Series([item['itemId'] for item in results])
+    df['Title'] = pd.Series([item['title'] for item in results])
+    df['Current Price'] = pd.Series([float(item['sellingStatus']['currentPrice']['value']) for item in results])
+    df['Location'] = pd.Series([item['location'] for item in results])
+    df['URL'] = pd.Series([item['viewItemURL'] for item in results])
+
     return df
