b8d11c67adb493ab8243238b55245460d7413f28
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-06-18 21:35:52 -0500

    Fixed the indentation level and added an if __name__ == '__main__' statement

--- hammond_query.py
+++ hammond_query.py
@@ -1,18 +1,22 @@
 query = {
-        'keywords': '+Hammond +(a100,""a-100"",B2,""B-2"",B3,""B-3"",C2,""C-2"",C3,""C-3"")',
-        'categoryId': '180010',
-        'itemFilter': [
-            {
-                'paramName': 'Currency',
-                'paramValue': 'USD',
-                'name': 'MaxPrice',
-                'value': '15000'
-            },
-            {
-                'paramName': 'Currency',
-                'paramValue': 'USD',
-                'name': 'MinPrice',
-                'value': '500'
-            },
-        ]
+    'keywords': '+Hammond +(a100,"a-100",B2,"B-2",B3,"B-3",C2,"C-2",C3,"C-3")',
+    'categoryId': '180010',
+    'itemFilter': [
+        {
+            'paramName': 'Currency',
+            'paramValue': 'USD',
+            'name': 'MaxPrice',
+            'value': '15000'
+        },
+        {
+            'paramName': 'Currency',
+            'paramValue': 'USD',
+            'name': 'MinPrice',
+            'value': '500'
+        },
+    ]
 }
+
+if __name__ == '__main__':
+    from pprint import pprint
+    pprint(query)
