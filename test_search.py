e0e05de492ccdec26be06a81fa8e6a37183b5d37
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-06-19 22:29:56 -0500

    Added import os to fix find_sensitive_path, renamed argument dir to directory to avoid conflict with built in name, converted comment to docstring

--- test_search.py
+++ test_search.py
@@ -3,6 +3,7 @@ from ebaysdk.finding import Connection as Finding
 from ebaysdk.exception import ConnectionError
 from accounts import PRODUCTION_APPID
 from ebay_processing import response_to_dataframe
+import os
 import search_queries
 
 # Use a variable to refer to the query and the output files to make it easier to manage multiple queries.
@@ -24,19 +25,20 @@ except ConnectionError as e:
     print(e)
     print(e.response.dict())
 
-# Perform a case insensitive search for the query file name.
-def find_sensitive_path(dir, insensitive_path):
 
+def find_sensitive_path(directory, insensitive_path):
+    """
+    Perform a case insensitive search for the query file name.
+    """
     insensitive_path = insensitive_path.strip(os.path.sep)
 
     parts = insensitive_path.split(os.path.sep)
     next_name = parts[0]
-    for name in os.listdir(dir):
+    for name in os.listdir(directory):
         if next_name.lower() == name.lower():
-            improved_path = os.path.join(dir, name)
+            improved_path = os.path.join(directory, name)
             if len(parts) == 1:
                 return improved_path
             else:
                 return find_sensitive_path(improved_path, os.path.sep.join(parts[1:]))
     return None
-    
