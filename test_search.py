9e6abff0a55282ae94b10edca5ef858286ae373f
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-06-19 22:21:47 -0500

    Uses the first argument to find the variable name from search_queries and assign to the query variable

--- test_search.py
+++ test_search.py
@@ -1,24 +1,22 @@
-import importlib
 import sys
 from ebaysdk.finding import Connection as Finding
 from ebaysdk.exception import ConnectionError
 from accounts import PRODUCTION_APPID
 from ebay_processing import response_to_dataframe
-from pprint import pprint
+import search_queries
 
-# Use a variable to refer to the query and the output files
-#  to make it easier to manage multiple queries.
-query_shortcut = sys.argv[1]
-
-query_file = importlib.import_module(query_filename)
+# Use a variable to refer to the query and the output files to make it easier to manage multiple queries.
+query_arg = sys.argv[1]
+# Converts the query argument into a variable called query
+exec('query = search_queries.' + query_arg)
 
 try:
     api = Finding(appid=PRODUCTION_APPID, config_file=None)
-    response = api.execute('findCompletedItems', query_file.query)
+    response = api.execute('findCompletedItems', query)
     df = response_to_dataframe(response)
     print(df.head())
     df.to_excel(
-        'Results_' + query_src + '.xlsx',
+        'Results_' + query_arg + '.xlsx',
         sheet_name='Results',
         index=False
     )
