2b050d92977c0548c03af4dad7d5961fc7ef1fab
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-06-19 21:57:27 -0500

    Fixed indentation, removed obsolete files

--- search_queries.py
+++ search_queries.py
@@ -1,94 +1,94 @@
 hammondB3 = {
-        'keywords': '+Hammond +(a100,"a-100",B2,"B-2",B3,"B-3",C2,"C-2",C3,"C-3")',
-        'categoryId': '180010',
-        'itemFilter': [
-            {
-                'paramName': 'Currency',
-                'paramValue': 'USD',
-                'name': 'MaxPrice',
-                'value': '15000'
-            },
-            {
-                'paramName': 'Currency',
-                'paramValue': 'USD',
-                'name': 'MinPrice',
-                'value': '500'
-            },
-        ]
+    'keywords': '+Hammond +(a100,"a-100",B2,"B-2",B3,"B-3",C2,"C-2",C3,"C-3")',
+    'categoryId': '180010',
+    'itemFilter': [
+        {
+            'paramName': 'Currency',
+            'paramValue': 'USD',
+            'name': 'MaxPrice',
+            'value': '15000'
+        },
+        {
+            'paramName': 'Currency',
+            'paramValue': 'USD',
+            'name': 'MinPrice',
+            'value': '500'
+        },
+    ]
 }
 
 hammondChopped = {
-        'keywords': '+Hammond +chopped',
-        'categoryId': '180010',
-        'itemFilter': [
-            {
-                'paramName': 'Currency',
-                'paramValue': 'USD',
-                'name': 'MaxPrice',
-                'value': '15000'
-            },
-            {
-                'paramName': 'Currency',
-                'paramValue': 'USD',
-                'name': 'MinPrice',
-                'value': '500'
-            },
-        ]
+    'keywords': '+Hammond +chopped',
+    'categoryId': '180010',
+    'itemFilter': [
+        {
+            'paramName': 'Currency',
+            'paramValue': 'USD',
+            'name': 'MaxPrice',
+            'value': '15000'
+        },
+        {
+            'paramName': 'Currency',
+            'paramValue': 'USD',
+            'name': 'MinPrice',
+            'value': '500'
+        },
+    ]
 }
 
 leslieTube = {
-        'keywords': '+leslie +(122,122R,122RV,122V,147,147RV,145,142,251,245,242,351)',
-        'categoryId': '180010',
-        'itemFilter': [
-            {
-                'paramName': 'Currency',
-                'paramValue': 'USD',
-                'name': 'MaxPrice',
-                'value': '2500'
-            },
-            {
-                'paramName': 'Currency',
-                'paramValue': 'USD',
-                'name': 'MinPrice',
-                'value': '150'
-            },
-        ]
+    'keywords': '+leslie +(122,122R,122RV,122V,147,147RV,145,142,251,245,242,351)',
+    'categoryId': '180010',
+    'itemFilter': [
+        {
+            'paramName': 'Currency',
+            'paramValue': 'USD',
+            'name': 'MaxPrice',
+            'value': '2500'
+        },
+        {
+            'paramName': 'Currency',
+            'paramValue': 'USD',
+            'name': 'MinPrice',
+            'value': '150'
+        },
+    ]
 }
 
 leslieSolidState = {
-        'keywords': '+leslie +(760,330,860,825,820,770,771)',
-        'categoryId': '180010',
-        'itemFilter': [
-            {
-                'paramName': 'Currency',
-                'paramValue': 'USD',
-                'name': 'MaxPrice',
-                'value': '2500'
-            },
-            {
-                'paramName': 'Currency',
-                'paramValue': 'USD',
-                'name': 'MinPrice',
-                'value': '100'
-            },
-        ]
+    'keywords': '+leslie +(760,330,860,825,820,770,771)',
+    'categoryId': '180010',
+    'itemFilter': [
+        {
+            'paramName': 'Currency',
+            'paramValue': 'USD',
+            'name': 'MaxPrice',
+            'value': '2500'
+        },
+        {
+            'paramName': 'Currency',
+            'paramValue': 'USD',
+            'name': 'MinPrice',
+            'value': '100'
+        },
+    ]
 }
 
 viscountLegend = {
-        'keywords': '+viscount +legend',
-        'categoryId': '180010',
-        'itemFilter': [
-            {
-                'paramName': 'Currency',
-                'paramValue': 'USD',
-                'name': 'MaxPrice',
-                'value': '15000'
-            },
-            {
-                'paramName': 'Currency',
-                'paramValue': 'USD',
-                'name': 'MinPrice',
-                'value': '500'
-            },
-        ]
+    'keywords': '+viscount +legend',
+    'categoryId': '180010',
+    'itemFilter': [
+        {
+            'paramName': 'Currency',
+            'paramValue': 'USD',
+            'name': 'MaxPrice',
+            'value': '15000'
+        },
+        {
+            'paramName': 'Currency',
+            'paramValue': 'USD',
+            'name': 'MinPrice',
+            'value': '500'
+        },
+    ]
 }
