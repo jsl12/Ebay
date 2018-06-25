663144184dfb821936beac6b4daa2d0d57c3a0d9
Jim Lancaster <jlancaster@mbdns.local>
2018-06-25 13:59:08 -0500

    Problems with postalCode, workarounds
    postalCode is throwing errors on some queries. I'm thinking it might be because the field is empty on some records. I tried going back to the original code to figure out how to add error checking. Created a new "ebaysdk_test" using the sdk's sample code.

--- ebaysdk_test.py
+++ ebaysdk_test.py
@@ -1,30 +1,23 @@
 import sys
 from ebaysdk.finding import Connection as Finding
 from ebaysdk.exception import ConnectionError
-from accounts import PRODUCTION_APPID
-from ebay_processing import response_to_dataframe
+import search_queries
 
 
-# Get the search query from the command line.
+# Select a search query from the command line. The search queries are defined in
+# search_queries.py
 query_arg = sys.argv[1]
-
 # Add error checking for empty string.
-
 # Converts the query argument into a variable called query
 exec('query = search_queries.' + query_arg.lower())
-
 # Add error checking to make sure query_arg matches something and that query isn't an empty string.
 
+
 try:
-    api = Finding(appid=PRODUCTION_APPID, config_file=None)
+    api = Finding()
     response = api.execute('findCompletedItems', query)
-    df = response_to_dataframe(response)
-    print(df.head())
-    df.to_excel(
-        'Results_' + query_arg + '.xlsx',
-        sheet_name='Results',
-        index=False
-    )
+    print(response.dict())
+
 except ConnectionError as e:
     print(e)
     print(e.response.dict())
